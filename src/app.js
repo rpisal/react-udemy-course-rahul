0b40fc5537f06cb7d66c09a859a37a3da90c9702
Rahul Pisal <rpisal@gmail.com>
2017-11-19 16:36:44 +0400

    complete code till section 3

--- src/app.js
+++ src/app.js
@@ -24,6 +24,12 @@ const onRemoveAll = () => {
     render();
 };
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+}
+
 const appRoot = document.getElementById("app");
 
 
@@ -33,13 +39,11 @@ const render = () => {
             <h1>{app.title}</h1> 
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {
-                    app.options.map((option) => {
-                        return <li key={option}>{option}</li>;
-                    })
+                    app.options.map((option) => <li key={option}>{option}</li>)
                 }
             </ol>
             <form onSubmit={onFormSubmit}>
---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,29 @@
+let visibility = false;
+
+const appRoot = document.getElementById("app");
+
+const toggleVisibility = () => {
+    visibility = !visibility;
+    render();
+};
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>
+                {visibility ? 'Hide details' : 'Show details'}
+            </button>
+            {visibility && (
+                <div>
+                    <p> Hey. These are some details you can now see</p>
+                </div>
+            )}
+        </div>        
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
+
