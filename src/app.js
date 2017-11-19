440430089b8ed88971f978769983d8c625adfe25
Rahul Pisal <rpisal@gmail.com>
2017-11-19 22:19:37 +0400

    tried state changes in the playground files - counter and build it visibility

--- src/app.js
+++ src/app.js
@@ -51,8 +51,13 @@ class Option extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll() {
-        alert('handleRemoveAll');
+        //alert('handleRemoveAll');
+        console.log(this.props.options);
     }
     render() {
         return (
