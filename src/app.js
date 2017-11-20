91fb83771c245a661e88a58804d28e49be0dd4ff
Rahul Pisal <rpisal@gmail.com>
2017-11-20 22:39:42 +0400

    code completed till section 6

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('app.js is running');
+//import './utils.js';
+// import subtract, { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(19, 45));
+// console.log(subtract(34, 45));
+
+// import isSenior, { isAdult, canDrink } from './person.js';
+// console.log(`is adult ${ isAdult(20)}`);
+// console.log(`can drink ${ isAdult(12)}`);
+// console.log(`is senior ${ isSenior(70)}`);
+
+// import validator from 'validator';
+
+// console.log(validator.isEmail('test@gmail.com'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
