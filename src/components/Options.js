d0137b146d19f431fd68c9b3325d125c791380ee
Rahul Pisal <rpisal@gmail.com>
2017-11-21 16:05:01 +0400

    section 8 related changes

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,21 +2,27 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-    <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started.</p>}
-        <ol>
-            {
-                props.options.map((option) => (
-                    <Option 
-                        key={option} 
-                        optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
-        </ol>
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
     </div>
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started.</p>}
+    <ol>
+      {props.options.map(option => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
+    </ol>
+  </div>
 );
 
 export default Options;
